2b714be9c20a1f0442c932c3e5883a6b14e042d8
Gabriel Sixel <gabriel.sixel@gmail.com>
2016-07-20 18:30:53 -0300

    hamming

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,6 +1,18 @@
 var Hamming = function () {};
-Hamming.prototype.compute = function(input) {
-  return true;
+
+  Hamming.prototype.compute = function(strandA, strandB) {
+    var lengthA = strandA.length;
+    var lengthB = strandB.length;
+    var distance = 0;
+    if (lengthA != lengthB) {
+      throw new Error('DNA strands must be of equal length.');
+    }
+    for (var i = 0; i < lengthA; i++) {
+      if(strandA.charAt(i) != strandB.charAt(i)) {
+        distance++;
+      }
+    }
+    return distance;
 };
 
 module.exports = Hamming;
