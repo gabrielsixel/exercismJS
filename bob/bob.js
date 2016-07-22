099c383ff73e1766d8d89bab4b442a2a05d12532
Gabriel Sixel <gabriel.sixel@gmail.com>
2016-07-21 21:02:43 -0300

    rna-transcription-done-first-try

---
+++ bob/bob.js
@@ -0,0 +1,14 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Bob = function() {};
+
+const Default = 'Whatever.';
+
+Bob.prototype.hey = function(input) {
+return Default;
+};
+
+module.exports = Bob;
