ff6f572132e57ca3006cae615aed6e019a2de71f
Gabriel Sixel <gabriel.sixel@gmail.com>
2016-07-27 21:31:29 -0300

    still not working

--- bob/bob.js
+++ bob/bob.js
@@ -5,13 +5,37 @@
 
 var Bob = function() {};
 
-const Default = 'Whatever.';
-const Shouting = 'Whoa, chill out!';
-const Question = 'Sure.';
-const Saying = 'Fine. Be that way!';
 
 Bob.prototype.hey = function(input) {
-return Default;
+  var length = input.length;
+  switch (true) {
+    //Asking a question, checks if the last character is a questiion mark
+    case input.charAt(length - 1) === '?':
+    return 'Sure.';
+    case (input === undefined || input === '') :
+    return 'Fine. Be that way!';
+    //Shouting checks if the sentence match the Regular Expression
+    case this.yelling(input) :
+    return 'Whoa, chill out!';
+    default:
+    return 'Whatever.';
+  }
+};
+
+Bob.prototype.yelling = function(input) {
+  var length = input.length;
+  var temp = '';
+  var regexUpper = new RegExp('([\u0041-\u1E921][A-Z-_:]+(?:(?:[^\r\n]\s[\u0041-\u1E921-_:]+)?)){1,}[^a-z]', 'gm');
+
+  for (var i = 0; i < length; i++) {
+    temp += input.charAt(i).toUpperCase();
+  }
+  if (temp !== input) {
+    if (!regexUpper.test(input)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 module.exports = Bob;
