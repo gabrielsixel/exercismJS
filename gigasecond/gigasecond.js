17dc82e96f329cf6d7957a9b1bdb4899d3444d25
Gabriel Sixel <gabriel.sixel@gmail.com>
2016-08-02 12:27:54 -0300

    Gigasecond working with the note to self to realy not use reserved words on the code

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,10 +1,11 @@
-
-
-var Gigasecond = function() {};
-
-Gigasecond.prototype.date = function() {
-
+var Gigasecond = function(date) {
+  this.startDate = date;
 };
 
+Gigasecond.prototype.date = function() {
+  var startTime = this.startDate.getTime();
+  var endDate = new Date(startTime + (Math.pow(10, 9) * 1000));
+  return endDate;
+};
 
 module.exports = Gigasecond;
