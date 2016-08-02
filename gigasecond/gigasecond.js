8708df1825ef807caa37647a9ae5099a43112904
Gabriel Sixel <gabriel.sixel@gmail.com>
2016-08-02 12:32:00 -0300

    Gigasecond one line prototype

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,11 +1,7 @@
 var Gigasecond = function(date) {
   this.startDate = date;
 };
-
 Gigasecond.prototype.date = function() {
-  var startTime = this.startDate.getTime();
-  var endDate = new Date(startTime + (Math.pow(10, 9) * 1000));
-  return endDate;
+  return new Date(this.startDate.getTime() + (Math.pow(10, 9) * 1000));
 };
-
 module.exports = Gigasecond;
